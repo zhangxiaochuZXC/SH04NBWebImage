5d18cf06230ddb14fbc9a6184928a24e18cd50e8
ITManager <zhangjiesharp@163.com>
2017-02-15 14:49:56 +0800

    自定义DownloaderOperation

---
+++ SHWebImage/SHWebImage/DSWebImage/DownloaderOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.h
+//  SHWebImage
+//
+//  Created by itcast on 17/2/15.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloaderOperation : NSOperation
+
+@end
---
+++ SHWebImage/SHWebImage/DSWebImage/DownloaderOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloaderOperation.m
+//  SHWebImage
+//
+//  Created by itcast on 17/2/15.
+//  Copyright © 2017年 itcast. All rights reserved.
+//
+
+#import "DownloaderOperation.h"
+
+@implementation DownloaderOperation
+
+@end
